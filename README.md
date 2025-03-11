8b439bfa54f86621af9f01215110883c1216c720
Nathan Gaul <nathanjgaul@gmail.com>
2025-03-11 09:06:33 -0500

    Update README with resources and examples; refactor sample texts into separate module

--- README.md
+++ README.md
@@ -1,8 +1,23 @@
 # standard_ebooks_tts
 
+## Resources
+
+- [GH hexgrad/kokoro](https://github.com/hexgrad/kokoro)
+- [HF hexgrad/Kokoro-82M-v1.1-zh](https://huggingface.co/hexgrad/Kokoro-82M-v1.1-zh)
+- [HF onnx-community/Kokoro-82M-v1.0-ONNX](https://huggingface.co/onnx-community/Kokoro-82M-v1.0-ONNX)
+- [Kokoro Voice Mixer](https://github.com/yvrjsharma/kokorotts-mixer)
+
+## Examples
+
+- [xenova/kokoro-web](https://github.com/xenova/kokoro-web)
+- [professor-kokoro-radio](https://github.com/ugotworms/professor-kokoro-radio)
+
 ## ToDos
 
-- [ ] Get [Kokoro v1](https://huggingface.co/hexgrad/Kokoro-82M) to produce sound
+- [X] Get [Kokoro v1](https://huggingface.co/hexgrad/Kokoro-82M) to produce sound
+- [ ] Integrate web worker
+- [ ] Voice mixing
+- [ ] Custome Voice Cloning/Training
 - [ ] Create custom interface for [Standard Ebooks](https://standardebooks.org/)
 - [ ] Chunk text from ebook one-page for TTS processing
 - [ ] End-to-End Pipeline: Search, Find book, Generate chunks of audiobook, combine into audiobook
--- src/routes/kokoro_demo/+page.svelte
+++ src/routes/kokoro_demo/+page.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
     import { KokoroTTS } from "kokoro-js";
     import { onMount } from "svelte";
+
+    import { sampleTexts } from "$lib/client/tts";
     
     let textToSpeak = $state('Life is like a box of chocolates. You never know what you\'re gonna get.');
     let tts = $state<KokoroTTS>();
@@ -8,20 +10,6 @@
     let isGenerating = $state(false);
     let availableVoices = $state<string[]>([]);
     let selectedVoice = $state("af_heart");
-    
-    // Sample texts for the random text button
-    const sampleTexts = [
-        'Life is like a box of chocolates. You never know what you\'re gonna get.',
-        'The quick brown fox jumps over the lazy dog.',
-        'To be or not to be, that is the question.',
-        'All that glitters is not gold.',
-        'The early bird catches the worm, but the second mouse gets the cheese.',
-        'A journey of a thousand miles begins with a single step.',
-        'In the middle of difficulty lies opportunity.',
-        'Be the change that you wish to see in the world.',
-        'The greatest glory in living lies not in never falling, but in rising every time we fall.',
-        'The way to get started is to quit talking and begin doing.'
-    ];
     
     onMount(() => {
         loadModel();
